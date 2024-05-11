0eb456fbe93f14f826c38f13a3e88a87088297bb
Millykapy <168423257+Millykapy@users.noreply.github.com>
2024-05-11 19:44:22 +0200

    added id and javascript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function refreshWeather(respond) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = respond.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "4o0269f4b7t3d5f7f0cfc4a0af394b27";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
